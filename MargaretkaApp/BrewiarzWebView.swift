6335ad4258f3483be69788354e6a222c82f4a42d
duszekjk <jacek@duszekjk.com>
2026-01-15 16:21:30 +0100

    Add Brewiarz web view wrapper

---
+++ MargaretkaApp/BrewiarzWebView.swift
@@ -0,0 +1,44 @@
+//
+//  BrewiarzWebView.swift
+//  MargaretkaApp
+//
+//  Created by Jacek Kałużny on 18/01/2026.
+//
+
+import SwiftUI
+import WebKit
+
+struct BrewiarzPrayerView: View {
+    let key: BrewiarzPrayerKey
+
+    @State private var resolvedURL: URL?
+
+    var body: some View {
+        ZStack {
+            if let url = resolvedURL {
+                WebView(url: url)
+            } else {
+                ProgressView("Ładowanie...")
+            }
+        }
+        .task(id: key) {
+            resolvedURL = await BrewiarzURLResolver.shared.resolveURL(for: key)
+        }
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let view = WKWebView()
+        view.allowsBackForwardNavigationGestures = true
+        return view
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard uiView.url != url else { return }
+        let request = URLRequest(url: url, cachePolicy: .reloadRevalidatingCacheData)
+        uiView.load(request)
+    }
+}
