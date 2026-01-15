a691e3e8d63c09f1820ae485504f81cee2e08ed5
duszekjk <jacek@duszekjk.com>
2026-01-15 11:38:54 +0100

    Add notification names for prayer actions

---
+++ MargaretkaApp/NotificationNames.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNames.swift
+//  MargaretkaApp
+//
+//  Created by Jacek Kałużny on 15/08/2025.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let prayerRestartRequested = Notification.Name("prayerRestartRequested")
+    static let prayerMarkDoneRequested = Notification.Name("prayerMarkDoneRequested")
+}
