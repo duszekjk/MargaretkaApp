e139d8f10540319034c550556d6105128515a68e
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-08-13 20:17:29 +0200

    Restore auto advance settings notification

--- MargaretkaApp/PrayerAutoAdvanceSettingsAlias.swift
+++ MargaretkaApp/PrayerAutoAdvanceSettingsAlias.swift
@@ -3,3 +3,7 @@ import SwiftUI
 struct PrayerAutoAdvanceSettingsView: View {
     var body: some View { PrayerAutoAdvanceCoreMLSettingsView() }
 }
+
+extension Notification.Name {
+    static let prayerAutoAdvancePreferencesChanged = Notification.Name("prayerAutoAdvancePreferencesChanged")
+}
