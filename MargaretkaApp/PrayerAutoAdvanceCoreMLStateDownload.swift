a76ab69ba033e0abb16e161c4e1eefcc60708c25
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-08-13 19:48:48 +0200

    Wire Core ML model download state

---
+++ MargaretkaApp/PrayerAutoAdvanceCoreMLStateDownload.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+extension PrayerAutoAdvanceCoreMLState {
+    func ensureModelAvailable() async -> Bool {
+        if model != nil { return true }
+        if isDownloading { return false }
+        isDownloading = true
+        defer { isDownloading = false }
+        do {
+            let downloaded = try await PrayerAutoAdvanceCoreMLDownloader.fetch()
+            try PrayerAutoAdvanceCoreMLInstall.run(downloaded, state: self)
+            lastError = nil
+            return model != nil
+        } catch {
+            lastError = error.localizedDescription
+            return false
+        }
+    }
+}
