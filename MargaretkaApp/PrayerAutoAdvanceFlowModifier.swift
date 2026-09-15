6be006f60d45dec9a8cac591b9a4ee9758c53326
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-09-15 16:13:33 +0200

    Register external display accessory from prayer flow

--- MargaretkaApp/PrayerAutoAdvanceFlowModifier.swift
+++ MargaretkaApp/PrayerAutoAdvanceFlowModifier.swift
@@ -16,6 +16,7 @@ struct PrayerAutoAdvanceFlowModifier: ViewModifier {
 
     func body(content: Content) -> some View {
         content
+            .prayerExternalDisplayAccessory()
             .onAppear { synchronizeContext() }
             .onDisappear {
                 controller.stop()
