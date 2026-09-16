4ea7d29a9ee2f74dd3460258132fe417931d29dd
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-09-16 12:36:17 +0200

    Add training audio archive preference

--- MargaretkaApp/PrayerAutoAdvanceStore.swift
+++ MargaretkaApp/PrayerAutoAdvanceStore.swift
@@ -3,4 +3,5 @@ import Foundation
 enum PrayerAutoAdvancePreferences {
     static let trainingEnabledKey = "prayerAutoAdvance.trainingEnabled"
     static let automaticEnabledKey = "prayerAutoAdvance.automaticEnabled"
+    static let archiveTrainingAudioKey = "prayerAutoAdvance.archiveTrainingAudio"
 }
