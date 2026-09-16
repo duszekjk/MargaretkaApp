44f605b3571234661d2c69d0ad645b0b877f92bd
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-09-16 12:36:39 +0200

    Archive optional training audio samples

--- MargaretkaApp/PrayerAutoAdvanceCoreMLRuntime.swift
+++ MargaretkaApp/PrayerAutoAdvanceCoreMLRuntime.swift
@@ -117,6 +117,7 @@ final class PrayerAutoAdvanceCoreMLRuntime: ObservableObject {
             postBoundaryDuration: PrayerAutoAdvanceTrainingPolicy.positiveWindow
         )
         let frozenPageAudio = pageAudioTransition.frozenPageAudio
+        PrayerTrainingAudioArchive.store(pageID: pageID, audio: frozenPageAudio)
 #else
         let swipeSpeech = PrayerAutoAdvanceSpeechSnapshot(transcript: "", lastSegmentEndTime: nil)
         let frozenPageAudio = PrayerAutoAdvanceAudioWindow(samples: [], sampleRate: 16_000)
